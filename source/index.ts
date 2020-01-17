a6ebfc2144bab7269e2284ae23a997b01015164d
narumincho <narumincho.starfy@gmail.com>
2020-01-17 10:47:20 +0900

    show body and useragen only and save data to firestore

--- source/index.ts
+++ source/index.ts
@@ -1,8 +1,13 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const app = admin.initializeApp();
+const firestore = app.firestore();
+const dataCollection = firestore.collection("data");
 
 export const api = functions.https.onRequest((request, response) => {
-  console.log("method", request.method);
-  console.log("headers", request.headers);
+  console.log("user-agent", request.headers["user-agent"]);
   console.log("body", request.body);
+  dataCollection.add(request.body);
   response.send("ok");
 });
